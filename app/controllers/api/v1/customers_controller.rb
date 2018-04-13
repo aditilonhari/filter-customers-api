b13eedc5d581d003738fc150a3f16399780732ee
Aditi Lonhari <lonhari.aditi@gmail.com>
2018-04-13 13:56:24 -0700

    Add filter by_full_name to backend

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,8 +1,18 @@
 module Api::V1
 	class CustomersController < ActionController::API
 		def index
-	      @customers = Customer.all
+		  if query_params[:by_full_name]
+		  	@customers = Customer.where("(firstname || lastname) LIKE ?",  "%#{query_params[:by_full_name]}%")
+		  else
+	      	@customers = Customer.all
+	      end
+
 	      render json: @customers
+	    end
+
+		private
+	    def query_params
+	    	params.permit(:by_full_name)
 	    end
 	end
 end
