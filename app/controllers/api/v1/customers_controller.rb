227bf9884fa6c20f730ae2849527b3ce7e86ad7f
Aditi Lonhari <lonhari.aditi@gmail.com>
2018-04-13 15:09:18 -0700

    Add filter by_company to backend api

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -7,12 +7,17 @@ module Api::V1
 	      	@customers = Customer.all
 	      end
 
+		  if query_params[:by_company]
+		  	id = Company.find_by_name(query_params[:by_company]).id
+		  	@customers = @customers.where(company_id: id)
+		  end
+
 	      render json: @customers
 	    end
 
 		private
 	    def query_params
-	    	params.permit(:by_full_name)
+	    	params.permit(:by_full_name, :by_company)
 	    end
 	end
 end
